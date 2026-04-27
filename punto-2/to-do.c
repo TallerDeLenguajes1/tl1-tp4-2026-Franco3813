27f3d1a78dd3fe5e41301bf53347873d0b3012a2
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-04-27 18:03:11 -0300

    punto 2 parte 2

--- punto-2/to-do.c
+++ punto-2/to-do.c
@@ -24,39 +24,86 @@ Nodo *crearTarea(int id){
     getchar();
     fgets(NuevaTarea->T.Descripcion,100,stdin);
         
-    printf("Ingrese la duracion en la tarea(10 - 100): ");
-    scanf("%d",&NuevaTarea->T.Duracion);
+    do{
+        printf("Ingrese la duracion en la tarea(10 - 100): ");
+        scanf("%d",&NuevaTarea->T.Duracion);
+    } while (NuevaTarea->T.Duracion < 10 || NuevaTarea->T.Duracion > 100);
+    
 
     NuevaTarea->T.TareaID = id;
-    id = id+1;
+    printf("numero de id: %d\n",NuevaTarea->T.TareaID);
 
     NuevaTarea->Siguiente = NULL;
 
     return NuevaTarea;
 }
-void InsertarAlFinal(Nodo *pendiente, Nodo *tarea){
-    Nodo *Aux = tarea;
-    while(Aux->Siguiente){
+void Insertar(Nodo **lista,Nodo *tarea){
+    if (*lista == NULL){
+        tarea->Siguiente = *lista;
+        *lista = tarea;
+    }else{
+        printf("La tarea ya a sido eliminada o no existe\n");
+    }
+    
+    
+}
+
+Nodo *Buscador(Nodo *pendiente,int idBuscado){
+    Nodo* Aux = pendiente;
+    while (Aux && Aux->T.TareaID != idBuscado){
         Aux = Aux->Siguiente;
     }
+    return Aux;
+}
 
-    Aux->Siguiente = tarea;
+Nodo *QuitarNodo(Nodo **lista,int id){
+    Nodo *nodoAux = (*lista);
+    Nodo *nodoAnt = NULL;
+
+    while (nodoAux != NULL && nodoAux->T.TareaID != id){
+        nodoAnt = nodoAux;
+        nodoAux = nodoAux->Siguiente;
+    }
+
+    if (nodoAux != NULL){
+        if (nodoAux == (*lista)){
+            (*lista) = nodoAux->Siguiente;
+        }else{
+            nodoAnt->Siguiente = nodoAux->Siguiente;
+        }
+        nodoAux->Siguiente = NULL;
+    }
+    
+    return nodoAux;
 }
 
 
 int main(){
-    int id = 1000,a;
-    Nodo *pendiente, *realizada,*tarea;
+    int id = 1000,a,idbuscado;
+    Nodo *pendiente, *realizada,*tarea,*buscado;
     
     pendiente = crearListaVacia();
     realizada = crearListaVacia();
     
     do{
         tarea = crearTarea(id);
-        InsertarAlFinal(pendiente,tarea);
+        Insertar(&pendiente,tarea);
         printf("Desea ingresar una nueva tarea?\n1)no \n2)si \nrespuesta: ");
         scanf("%d",&a);
+        id = id+1;
     } while (a == 2);
+    printf("\n");
+    do{
+        printf("Ingrese el id la tarea completada: ");
+        scanf("%d",&idbuscado);
+        buscado = Buscador(pendiente,idbuscado);
+        Insertar(&realizada,buscado);
+        QuitarNodo(&pendiente,idbuscado);
+        printf("Desea agregar otra tarea?\n1)no \n2)si \nrespuesta: ");
+        scanf("%d",&a);
+
+    } while (a == 2);
+    
     
 
 }
