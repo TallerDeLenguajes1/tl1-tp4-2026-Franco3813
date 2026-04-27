9afa0461d0383404312af43e07ba014a37d5679a
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-04-27 15:55:20 -0300

    punto 2 parte 1

--- punto-2/to-do.c
+++ punto-2/to-do.c
@@ -12,21 +12,51 @@ typedef struct Nodo{
     struct Nodo *Siguiente;
 }Nodo;
 
-Nodo *crearlista(){
+Nodo *crearListaVacia(){
     return NULL;
 }
 
-int main(){
-    int repetir = 1;
-    Nodo *pendiente, *realizada;
+Nodo *crearTarea(int id){
+    Nodo *NuevaTarea = (Nodo*)malloc(sizeof(Nodo));
+    NuevaTarea->T.Descripcion = malloc(100 * sizeof(char));
     
-    pendiente = crearlista();
-    realizada = crearlista();
+    printf("Ingrese la descripcion de la nueva tarea: ");
+    getchar();
+    fgets(NuevaTarea->T.Descripcion,100,stdin);
+        
+    printf("Ingrese la duracion en la tarea(10 - 100): ");
+    scanf("%d",&NuevaTarea->T.Duracion);
+
+    NuevaTarea->T.TareaID = id;
+    id = id+1;
+
+    NuevaTarea->Siguiente = NULL;
+
+    return NuevaTarea;
+}
+void InsertarAlFinal(Nodo *pendiente, Nodo *tarea){
+    Nodo *Aux = tarea;
+    while(Aux->Siguiente){
+        Aux = Aux->Siguiente;
+    }
+
+    Aux->Siguiente = tarea;
+}
+
+
+int main(){
+    int id = 1000,a;
+    Nodo *pendiente, *realizada,*tarea;
+    
+    pendiente = crearListaVacia();
+    realizada = crearListaVacia();
     
     do{
-        
-        printf("\nIngrese una nueva tarea: ");
-
-    } while (repetir == 2);
+        tarea = crearTarea(id);
+        InsertarAlFinal(pendiente,tarea);
+        printf("Desea ingresar una nueva tarea?\n1)no \n2)si \nrespuesta: ");
+        scanf("%d",&a);
+    } while (a == 2);
     
+
 }
