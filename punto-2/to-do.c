29fbc1ff3ddf6386c4495468a1aa4c0dd8d7828e
Franco3813 <franconicolasaguirre.04@gmail.com>
2026-04-27 22:09:35 -0300

    Punto 2 terminado

--- punto-2/to-do.c
+++ punto-2/to-do.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef struct Tarea{
     int TareaID;//Numérico autoincremental comenzando en 1000
@@ -42,12 +43,32 @@ void Insertar(Nodo **lista,Nodo *tarea){
     *lista = tarea;
 }
 
-Nodo *Buscador(Nodo *pendiente,int idBuscado){
-    Nodo* Aux = pendiente;
-    while (Aux && Aux->T.TareaID != idBuscado){
+void BuscadorPorId(Nodo *lista,Nodo *lista2,int id){
+    Nodo *Aux = lista;
+    Nodo *Aux2 = lista2;
+
+    while (Aux && Aux->T.TareaID != id){
         Aux = Aux->Siguiente;
     }
-    return Aux;
+
+    while (Aux2 && Aux2->T.TareaID != id){
+        Aux2 = Aux2->Siguiente;
+    }
+    if (Aux2 == NULL){
+        printf("\nDescripcion de la tarea: %s", Aux->T.Descripcion);
+        printf("\nDuracion de la tarea: %d", Aux->T.Duracion);
+        printf("\nId de la tarea: %d", Aux->T.TareaID);
+        printf("\nUbicacion de la tarea: Pendiente");
+        printf("\n------------------------\n");
+    }else{
+        printf("\nDescripcion de la tarea: %s", Aux2->T.Descripcion);
+        printf("\nDuracion de la tarea: %d", Aux2->T.Duracion);
+        printf("\nId de la tarea: %d", Aux2->T.TareaID);
+        printf("\nUbicacion de la tarea: Realizada");
+        printf("\n------------------------\n");
+    }
+    
+    
 }
 
 Nodo *QuitarNodo(Nodo **lista,int id){
@@ -85,10 +106,39 @@ void mostrarLista(Nodo *lista){
     
 }
 
+void BuscardorPorPalabra(Nodo *lista,Nodo *lista2, char *frase){
+    Nodo *Aux = lista;
+    Nodo *Aux2 = lista2;
+
+    while (Aux != NULL){
+        if (strstr(Aux->T.Descripcion, frase) != NULL){
+            printf("\nDescripcion de la tarea: %s", Aux->T.Descripcion);
+            printf("\nDuracion de la tarea: %d", Aux->T.Duracion);
+            printf("\nId de la tarea: %d", Aux->T.TareaID);
+            printf("\nUbicacion de la tarea: Pendiente");
+            printf("\n------------------------\n");
+        }
+        Aux = Aux->Siguiente;
+    }
+
+    while (Aux2 != NULL){
+        if (strstr(Aux2->T.Descripcion, frase) != NULL){
+            printf("\nDescripcion de la tarea: %s", Aux2->T.Descripcion);
+            printf("\nDuracion de la tarea: %d", Aux2->T.Duracion);
+            printf("\nId de la tarea: %d", Aux2->T.TareaID);
+            printf("\nUbicacion de la tarea: Realizada");
+            printf("\n------------------------\n");
+        }
+        Aux2 = Aux2->Siguiente;
+    }
+    
+}
+
 
 int main(){
-    int id = 1000,a,idbuscado;
-    Nodo *pendiente, *realizada,*tarea,*buscado;
+    int id = 1000,a,idbuscado,b,c;
+    Nodo *pendiente, *realizada,*tarea,*buscado,*Aux;
+    char frase[100];
     
     pendiente = crearListaVacia();
     realizada = crearListaVacia();
@@ -116,4 +166,20 @@ int main(){
     printf("\n");
     printf("\nLista de las tareas realizadas");
     mostrarLista(realizada);
+
+    printf("\nBusqueda de tarea");
+    printf("\n1)Buscar por palabra clave \n2)Buscar por id");
+    printf("\nRespuesta: ");
+    scanf("%d",&b);
+
+    if (b == 1){
+        printf("\nIngrese una palabra clave para buscar un nombre: ");
+        scanf("%s",frase);
+        BuscardorPorPalabra(pendiente,realizada,frase);
+    }else{
+        printf("\nIngrese el id de la tarea: ");
+        scanf("%d",&c);
+        BuscadorPorId(pendiente,realizada,c);
+    }
+    
 }
